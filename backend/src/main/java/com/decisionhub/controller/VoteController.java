31a2de824a862c526496ea3d588cc36a1787e033
keerthana12hv <jkeerthana925@gmail.com>
2026-07-01 12:29:52 +0530

    Initial project setup

---
+++ backend/src/main/java/com/decisionhub/controller/VoteController.java
@@ -0,0 +1,10 @@
+package com.decisionhub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vote")
+public class VoteController {
+
+}
